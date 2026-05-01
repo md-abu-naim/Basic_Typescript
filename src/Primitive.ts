3b371d6fe2b39cdfbf98c555ba386dafc3edabb3
abunaim34 <farhanadnanfarabi@gmail.com>
2026-05-01 08:51:20 +0600

    TS data types

---
+++ src/Primitive.ts
@@ -0,0 +1,29 @@
+// JS Data type
+// string, number, boolean, undefind, null
+
+
+// TS Data type
+// string, number, boolean, undefind, null, naver, unkwon, void
+
+
+let userName = 'Naim113'
+
+// userName = 12 // error
+userName = 'naim522' // right
+
+let userId = 133
+
+// userId = 'sfsa' // error
+
+userId = 5226 // right
+
+let isAdmin = false
+
+// isAdmin = 'true'  // error
+isAdmin = true  // right
+
+
+let x = undefined
+
+x = 'abar'
+x = 123
